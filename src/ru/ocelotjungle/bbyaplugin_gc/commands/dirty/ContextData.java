106f75d3a1a993c6b68a76d6697e6463ce23dc3f
Nikita Medvedev <some.redteapot@gmail.com>
2023-01-15 00:27:29 +0700

    Update the plugin to Minecraft 1.19.3

--- src/ru/ocelotjungle/bbyaplugin_gc/commands/dirty/ContextData.java
+++ src/ru/ocelotjungle/bbyaplugin_gc/commands/dirty/ContextData.java
@@ -4,8 +4,8 @@ import net.minecraft.commands.CommandListenerWrapper;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.EntityPlayer;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_18_R2.command.CraftBlockCommandSender;
-import org.bukkit.craftbukkit.v1_18_R2.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_19_R2.command.CraftBlockCommandSender;
+import org.bukkit.craftbukkit.v1_19_R2.entity.CraftPlayer;
 
 public class ContextData {
     private final CommandSender sender;
@@ -18,9 +18,9 @@ public class ContextData {
             this.commandListenerWrapper = senderCast.getWrapper();
         } else if(sender instanceof CraftPlayer) {
             EntityPlayer senderCast = ((CraftPlayer) sender).getHandle();
-            this.commandListenerWrapper = senderCast.cQ();
+            this.commandListenerWrapper = senderCast.cY();
         } else {
-            this.commandListenerWrapper = MinecraftServer.getServer().aB();
+            this.commandListenerWrapper = MinecraftServer.getServer().aC();
         }
     }
 
